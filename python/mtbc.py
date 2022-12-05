961ca4fc6d0c0f472f45cd2014944a0c4f55f805
darthos <clement_freedom@hotmail.fr>
2022-12-05 17:25:34 +0100

    a

--- python/mtbc.py
+++ python/mtbc.py
@@ -1,12 +1,9 @@
-from Bio import Entrez
-import xml.etree.ElementTree as ET
+import random
+from operator import itemgetter
+
 import pandas as pd
 import xmltodict
-from operator import itemgetter
-from operator import methodcaller
-import random
-import re
-import json
+from Bio import Entrez
 
 
 def mtbc_random():
@@ -19,23 +16,24 @@ def mtbc_random():
 
     sample_list = random.choices(record['IdList'], k=10)
 
-    handle_epost = Entrez.epost(db="sra", id=(",").join(map(str,sample_list)), retmax=retmax)
+    handle_epost = Entrez.epost(db="sra", id=",".join(map(str, sample_list)), retmax=retmax)
     record_epost = Entrez.read(handle_epost)
     handle_epost.close()
 
-    handle_esummary2 = Entrez.esummary(db="sra", webenv = record_epost['WebEnv'], query_key = record_epost['QueryKey'],retmax=retmax )
+    handle_esummary2 = Entrez.esummary(db="sra", webenv=record_epost['WebEnv'], query_key=record_epost['QueryKey'],
+                                       retmax=retmax)
     record_esummary2 = Entrez.read(handle_esummary2)
     handle_esummary2.close()
 
     runs = list(map(itemgetter('Runs'), record_esummary2))
 
-    runs_1= list(map(lambda run: "<root>" + run + "</root>", runs))
+    runs_1 = list(map(lambda run: "<root>" + run + "</root>", runs))
 
     runs_acc = list(map(xmltodict.parse, runs_1))
 
-    ACC_LIST = pd.json_normalize(runs_acc)['root.Run.@acc']
-    return ACC_LIST
+    acc_list = pd.json_normalize(runs_acc)['root.Run.@acc']
+    return acc_list
+
 
 if __name__ == "__main__":
     mtbc_random()
-
