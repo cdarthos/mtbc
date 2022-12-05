6a26ac40f098a40bd933da179425eff001dc181e
darthos <clement_freedom@hotmail.fr>
2022-12-05 18:38:22 +0100

    class Mtbc_random def mtbc_random_search_id(self): def mtbc_random_get_accession_number(self):

--- python/mtbc.py
+++ python/mtbc.py
@@ -1,39 +1,62 @@
 import random
 from operator import itemgetter
-
 import pandas as pd
 import xmltodict
 from Bio import Entrez
 
 
-def mtbc_random():
-    retmax = 1000000
-    Entrez.email = "Your.Name.Here@example.org"
+class Mtbc_random:
+    def __init__(self):
+        self.retmax = 1000000
+        self.random_number = 10
+        Entrez.email = 'A.N.Other@example.com'
+        self.base_taxa = 'txid77643[ORGN]'
 
-    handle = Entrez.esearch(db="sra", term="txid77643[ORGN]", retmax=retmax, retstart=0)
-    record = Entrez.read(handle)
-    handle.close()
 
-    sample_list = random.choices(record['IdList'], k=10)
+    def mtbc_random_search_id(self):
 
-    handle_epost = Entrez.epost(db="sra", id=",".join(map(str, sample_list)), retmax=retmax)
-    record_epost = Entrez.read(handle_epost)
-    handle_epost.close()
+        handle = Entrez.esearch(db="sra",
+                                term=self.base_taxa,
+                                retmax= self.retmax,
+                                retstart=0)
+        record = Entrez.read(handle)
+        handle.close()
 
-    handle_esummary2 = Entrez.esummary(db="sra", webenv=record_epost['WebEnv'], query_key=record_epost['QueryKey'],
-                                       retmax=retmax)
-    record_esummary2 = Entrez.read(handle_esummary2)
-    handle_esummary2.close()
+        return record['IdList']
 
-    runs = list(map(itemgetter('Runs'), record_esummary2))
+    def mtbc_random_get_accession_number(self):
+        id_list = self.mtbc_random_search_id()
+        sample_list = random.choices(id_list,
+                                     k=self.random_number)
 
-    runs_1 = list(map(lambda run: "<root>" + run + "</root>", runs))
+        handle_epost = Entrez.epost(db="sra",
+                                    id=",".join(map(str, sample_list)),
+                                    retmax=self.retmax)
+        record_epost = Entrez.read(handle_epost)
+        handle_epost.close()
 
-    runs_acc = list(map(xmltodict.parse, runs_1))
+        handle_esummary = Entrez.esummary(db="sra",
+                                          webenv=record_epost['WebEnv'],
+                                          query_key=record_epost['QueryKey'],
+                                          retmax=self.retmax)
+        record_esummary = Entrez.read(handle_esummary)
+        handle_esummary.close()
 
-    acc_list = pd.json_normalize(runs_acc)['root.Run.@acc']
-    return acc_list
+        runs = list(map(itemgetter('Runs'),
+                        record_esummary))
+
+        runs_1 = list(map(lambda run: "<root>" + run + "</root>",
+                          runs))
+
+        runs_acc = list(map(xmltodict.parse, runs_1))
+
+        acc_list = pd.json_normalize(runs_acc)['root.Run.@acc']
+        return acc_list.to_list()
 
 
 if __name__ == "__main__":
-    mtbc_random()
+    mtbc = Mtbc_random()
+    print(mtbc.mtbc_random_search_id()[:10])
+    print()
+    print(mtbc.mtbc_random_get_accession_number())
+
