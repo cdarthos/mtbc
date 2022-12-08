9401fc5bad1a4e1388286973a2135145d207f698
darthos <clement_freedom@hotmail.fr>
2022-12-08 21:51:07 +0100

    ncbi request + mtbc request + reconstruct SNP alignement + tree construction

--- python/test_mtbc.py
+++ python/test_mtbc.py
@@ -6,9 +6,9 @@ from mtbc import MtbcRandom
 class TestMtbcRandom(TestCase):
 
     def test_construct_search_request(self):
-        mtbc = MtbcRandom()
-        mtbc_78331 = MtbcRandom(select_mycobacterium_mungi=True)
-        mtbc_78331_1844474 = MtbcRandom(select_mycobacterium_mungi=True, select_mycobacterium_canettii=True)
+        mtbc = MtbcRandom(list_length=20)
+        mtbc_78331 = MtbcRandom(select_mycobacterium_mungi=True,list_length=20)
+        mtbc_78331_1844474 = MtbcRandom(select_mycobacterium_mungi=True, select_mycobacterium_canettii=True,list_length=20)
         self.assertEqual(mtbc.ncbi_request_all_id, 'txid77643[ORGN]')
         self.assertEqual(mtbc_78331.ncbi_request_all_id, 'txid1844474[ORGN]')
         self.assertEqual(mtbc_78331_1844474.ncbi_request_all_id, 'txid78331[ORGN] OR txid1844474[ORGN]')
