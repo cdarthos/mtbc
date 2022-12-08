fffd88a95cc4353f852234df0336c7a19872306e
darthos <clement_freedom@hotmail.fr>
2022-12-08 14:35:16 +0100

    taxa1

--- python/mtbc.py
+++ python/mtbc.py
@@ -1,8 +1,13 @@
+import json
 import random
 from operator import itemgetter
+
+import pandas
 import pandas as pd
 import xmltodict
 from Bio import Entrez
+import requests
+import numpy as np
 
 
 class MtbcRandom:
@@ -21,34 +26,64 @@ class MtbcRandom:
     # Mycobacterium tuberculosis 1773
     taxa_mycobacterium_tuberculosis = '1773'
 
+    sequence = {}
+    sequence['NC_000962.3'] = {}
+    alignement = {}
+
+
+
     def __init__(self,
                  select_mycobacterium_canettii=False,
                  select_mycobacterium_mungi=False,
                  select_mycobacterium_orygis=False,
                  select_mycobacterium_tuberculosis=False,
-                 ):
-        self.retmax = 1000000
-        self.random_number = 10
+                 retmax=10000000,
+                 list_length=10,
+                 debug=False):
+        """
+        @param retmax:
+        @param list_length:
+        @param select_mycobacterium_canettii:
+        @param select_mycobacterium_mungi:
+        @param select_mycobacterium_orygis:
+        @param select_mycobacterium_tuberculosis:
+        """
+
+        self.df_mutation = None
+        self.debug = debug
+        self.retmax = retmax
+        self.list_length = list_length
         Entrez.email = 'A.N.Other@example.com'
         self.select_taxa = {self.taxa_mycobacterium_canettii: select_mycobacterium_canettii,
                             self.taxa_mycobacterium_mungi: select_mycobacterium_mungi,
                             self.taxa_mycobacterium_orygis: select_mycobacterium_orygis,
                             self.taxa_mycobacterium_tuberculosis: select_mycobacterium_tuberculosis}
+        self.req = self.construct_search_request()
+        self.id_list = self.mtbc_random_search_id()
+        self.acc_list = self.mtbc_random_get_accession_number()
+        self.request()
+        #if debug:
+        #    print(json.dumps(self.sequence,indent=4))
+        #    print(self.sequence.keys())
+        self.reconstruct_alignement()
 
     def construct_search_request(self):
-
         if True in self.select_taxa.values():
             request = " OR ".join('txid' + key + '[ORGN]' for key, value in self.select_taxa.items() if value)
+            if self.debug:
+                print(request)
             return request
 
         else:
             request = 'txid' + self.taxa_mycobacterium_tuberculosis_complex + '[ORGN]'
+            if self.debug:
+                print(request)
             return request
 
     def mtbc_random_search_id(self):
         retmax = 1000000
         handle = Entrez.esearch(db="sra",
-                                term=self.construct_search_request(),
+                                term=self.req,
                                 retmax=retmax,
                                 retstart=0)
         record = Entrez.read(handle)
@@ -57,9 +92,9 @@ class MtbcRandom:
         return record['IdList']
 
     def mtbc_random_get_accession_number(self):
-        id_list = self.mtbc_random_search_id()
+        id_list = self.id_list
         sample_list = random.choices(id_list,
-                                     k=self.random_number)
+                                     k=self.list_length)
 
         handle_epost = Entrez.epost(db="sra",
                                     id=",".join(map(str, sample_list)),
@@ -85,12 +120,54 @@ class MtbcRandom:
         acc_list = pd.json_normalize(runs_acc)['root.Run.@acc']
         return acc_list.to_list()
 
+    def request(self):
+        result_alignement = {}
+        for sra in self.acc_list:
+
+            head = {'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'gnksoftware.synology.me:30002'}
+            parameters = {
+                'search': '{"type":"boolean","quantifier":"allOf","filters":[{"type":"terms","terms":["' + \
+                          str(sra) + \
+                          '"],"attribute":"publicId.keyword","quantifier":"anyOf"}]}',
+                'alignmentParams': '{"types":["snp"],"includeDataInComment":true}'}
+            url = 'http://gnksoftware.synology.me:30002/strains/alignment'
+            r = requests.post(url, parameters, head)
+
+            if len(r.text) != 0:
+                self.sequence[r.text[1:].split("\n")[0]] = {}
+                for diff in r.text.split("\n")[2:]:
+                    if len(diff) != 0:
+                        ###############
+                        # premiere etape, on selectionne juste les mutations (pas d'indels/insertion)
+                        ###############
+                        if len(diff.split(":")[3]) == 1:
+                            self.sequence['NC_000962.3'][diff.split(":")[1]] = diff.split(":")[2]
+                            self.sequence[r.text[1:].split("\n")[0]][diff.split(":")[1]] = diff.split(":")[3]
+                key = r.text[1:].split("\n")[0]
+                value = r.text[1:].split("\n")[1]
+                result_alignement[key] = [len(value), value]
+
+        return result_alignement
+
+    def reconstruct_alignement(self):
+        df_mutation = pandas.DataFrame.from_dict(self.sequence)
+        df_ref = pandas.Series(df_mutation['NC_000962.3'])
+        for column in df_mutation.columns:
+            df_mutation[column] = df_mutation[column].fillna(df_ref, axis=0)
+
+        if self.debug:
+            print(df_mutation)
+
+
 
 if __name__ == "__main__":
-    mtbc = MtbcRandom(select_mycobacterium_canettii=True, select_mycobacterium_mungi=True)
-    print(mtbc.construct_search_request())
-    print()
-    print(mtbc.mtbc_random_search_id()[:10])
-    print()
-    print(mtbc.mtbc_random_get_accession_number())
-    print()
+    mtbc = MtbcRandom(debug=True)
+    # print(mtbc.construct_search_request())
+    # print()
+    # print(mtbc.id_list[:10])
+    # print()
+    # print(mtbc.acc_list)
+    # print()
+    # print(json.dumps(mtbc.alignement, indent=4))
+
--- python/test_mtbc.py
+++ python/test_mtbc.py
@@ -9,9 +9,9 @@ class TestMtbcRandom(TestCase):
         mtbc = MtbcRandom()
         mtbc_78331 = MtbcRandom(select_mycobacterium_mungi=True)
         mtbc_78331_1844474 = MtbcRandom(select_mycobacterium_mungi=True, select_mycobacterium_canettii=True)
-        self.assertEqual(mtbc.construct_search_request(), 'txid77643[ORGN]')
-        self.assertEqual(mtbc_78331.construct_search_request(), 'txid1844474[ORGN]')
-        self.assertEqual(mtbc_78331_1844474.construct_search_request(), 'txid78331[ORGN] OR txid1844474[ORGN]')
+        self.assertEqual(mtbc.req, 'txid77643[ORGN]')
+        self.assertEqual(mtbc_78331.req, 'txid1844474[ORGN]')
+        self.assertEqual(mtbc_78331_1844474.req, 'txid78331[ORGN] OR txid1844474[ORGN]')
     #
     # def test_mtbc_random_search_id(self):
     #     self.fail()
