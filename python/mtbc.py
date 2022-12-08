594742cb7f95f0a88fff12ee207206af84e65808
darthos <clement_freedom@hotmail.fr>
2022-12-08 20:48:25 +0100

    ncbi request + mtbc request + reconstruct SNP alignement + tree construction

--- python/mtbc.py
+++ python/mtbc.py
@@ -1,13 +1,13 @@
-import json
 import random
 from operator import itemgetter
 
 import pandas
 import pandas as pd
+import requests
 import xmltodict
 from Bio import Entrez
-import requests
-import numpy as np
+from Bio import Phylo, AlignIO
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 
 
 class MtbcRandom:
@@ -26,11 +26,8 @@ class MtbcRandom:
     # Mycobacterium tuberculosis 1773
     taxa_mycobacterium_tuberculosis = '1773'
 
-    sequence = {}
-    sequence['NC_000962.3'] = {}
+    sequence = {'NC_000962.3': {}}
     alignement = {}
-
-
 
     def __init__(self,
                  select_mycobacterium_canettii=False,
@@ -38,7 +35,7 @@ class MtbcRandom:
                  select_mycobacterium_orygis=False,
                  select_mycobacterium_tuberculosis=False,
                  retmax=10000000,
-                 list_length=10,
+                 list_length=100,
                  debug=False):
         """
         @param retmax:
@@ -49,6 +46,7 @@ class MtbcRandom:
         @param select_mycobacterium_tuberculosis:
         """
 
+        self.align = None
         self.df_mutation = None
         self.debug = debug
         self.retmax = retmax
@@ -59,13 +57,15 @@ class MtbcRandom:
                             self.taxa_mycobacterium_orygis: select_mycobacterium_orygis,
                             self.taxa_mycobacterium_tuberculosis: select_mycobacterium_tuberculosis}
         self.req = self.construct_search_request()
-        self.id_list = self.mtbc_random_search_id()
+        self.id_list = self.get_all_id()
         self.acc_list = self.mtbc_random_get_accession_number()
         self.request()
-        #if debug:
+        # if debug:
         #    print(json.dumps(self.sequence,indent=4))
         #    print(self.sequence.keys())
-        self.reconstruct_alignement()
+        self.reconstruct_sequence()
+        self.align_reconstruct()
+        self.create_nj_tree()
 
     def construct_search_request(self):
         if True in self.select_taxa.values():
@@ -80,13 +80,15 @@ class MtbcRandom:
                 print(request)
             return request
 
-    def mtbc_random_search_id(self):
+    def get_all_id(self):
         retmax = 1000000
         handle = Entrez.esearch(db="sra",
                                 term=self.req,
                                 retmax=retmax,
                                 retstart=0)
         record = Entrez.read(handle)
+        if self.debug:
+            print(record)
         handle.close()
 
         return record['IdList']
@@ -100,6 +102,8 @@ class MtbcRandom:
                                     id=",".join(map(str, sample_list)),
                                     retmax=self.retmax)
         record_epost = Entrez.read(handle_epost)
+        if self.debug:
+            print(record_epost)
         handle_epost.close()
 
         handle_esummary = Entrez.esummary(db="sra",
@@ -107,6 +111,8 @@ class MtbcRandom:
                                           query_key=record_epost['QueryKey'],
                                           retmax=self.retmax)
         record_esummary = Entrez.read(handle_esummary)
+        if self.debug:
+            print(record_esummary)
         handle_esummary.close()
 
         runs = list(map(itemgetter('Runs'),
@@ -127,8 +133,8 @@ class MtbcRandom:
             head = {'Content-Type': 'application/x-www-form-urlencoded',
                     'Host': 'gnksoftware.synology.me:30002'}
             parameters = {
-                'search': '{"type":"boolean","quantifier":"allOf","filters":[{"type":"terms","terms":["' + \
-                          str(sra) + \
+                'search': '{"type":"boolean","quantifier":"allOf","filters":[{"type":"terms","terms":["' +
+                          str(sra) +
                           '"],"attribute":"publicId.keyword","quantifier":"anyOf"}]}',
                 'alignmentParams': '{"types":["snp"],"includeDataInComment":true}'}
             url = 'http://gnksoftware.synology.me:30002/strains/alignment'
@@ -150,15 +156,30 @@ class MtbcRandom:
 
         return result_alignement
 
-    def reconstruct_alignement(self):
+    def reconstruct_sequence(self):
         df_mutation = pandas.DataFrame.from_dict(self.sequence)
         df_ref = pandas.Series(df_mutation['NC_000962.3'])
-        for column in df_mutation.columns:
-            df_mutation[column] = df_mutation[column].fillna(df_ref, axis=0)
-
         if self.debug:
             print(df_mutation)
+        with open('alignement/alignement.fasta', 'w') as writer:
+            for column in df_mutation.columns:
+                df_mutation[column] = df_mutation[column].fillna(df_ref, axis=0)
+                print(">" + column)
+                print("".join(df_mutation[column].to_list()))
+                writer.writelines(">" + column + "\n")
+                writer.writelines("".join(df_mutation[column].to_list()) + "\n")
 
+    def align_reconstruct(self):
+        self.align = AlignIO.read('alignement/alignement.fasta', 'fasta')
+        if self.debug:
+            print(self.align)
+
+    def create_nj_tree(self):
+        calculator = DistanceCalculator('identity')
+        dist_matrix = calculator.get_distance(self.align)
+        constructor = DistanceTreeConstructor()
+        nj_tree = constructor.nj(dist_matrix)
+        Phylo.write(nj_tree, "tree/tree1.nwk", "newick")
 
 
 if __name__ == "__main__":
@@ -170,4 +191,3 @@ if __name__ == "__main__":
     # print(mtbc.acc_list)
     # print()
     # print(json.dumps(mtbc.alignement, indent=4))
-
