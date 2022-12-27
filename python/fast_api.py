b588586d37fdb71dafc29c7b27ccedb9bbcc135a
cdarthos <90338419+cdarthos@users.noreply.github.com>
2022-12-27 14:51:12 +0100

    Update fast_api.py

--- python/fast_api.py
+++ python/fast_api.py
@@ -83,9 +83,10 @@ async def fasta_align(debug: bool = False,
 @test.get("/mtbc_fasta_align_from_json")
 async def fasta_align_from_json(json_file: str = ""):
     with open("request/{0}".format(json_file), 'r') as json_request:
-        mtbc_json = json.load(json_request)
+        mtbc_json = json.loads(json_request, object_hook=mtbc_ncbi.MtbcGetRandomSRA)
     print(mtbc_json)
-    mtbc_inst = mtbc_ncbi.MtbcGetRandomSRA(mtbc_json)
+    mtbc_inst = mtbc_json
+    print(mtbc_inst)
     mtbc_fasta = mtbc_tools.MtbcAcclistToFASTA(mtbc_inst)
     mtbc_fasta.align_reconstruct()
     return FileResponse(path='alignement/{0}'.format(mtbc_fasta.id), media_type='text/plain',
