4c2c534f7ee6748d345596b582359bc9b9bc6c29
darthos <clement_freedom@hotmail.fr>
2023-01-05 22:43:16 +0100

    Dockerfile + fastapi

--- python/fast_api.py
+++ python/fast_api.py
@@ -254,7 +254,7 @@ async def nj_tree_from_db(id: str = ""):
 
             fasta_ = fasta_align_from_json(id)
             logging.info(fasta_)
-            fasta = fasta_.body
+            fasta = fasta_.body.decode("utf-8")
             logging.debug(fasta)
             #await fasta_coroutine
             #try:
